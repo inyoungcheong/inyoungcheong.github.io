8118c75236e1eb156f9543d9e58bd367221dbf6c
inyoungcheong <44386633+inyoungcheong@users.noreply.github.com>
2023-10-29 22:59:59 +0000

    Deploying to gh-pages from @ inyoungcheong/inyoungcheong.github.io@76c6912b8f12e4a253b39b3ad57c4b7ebd59c32e 🚀

---
+++ assets/css/script.js
@@ -0,0 +1,1 @@
+function rotateText(){rows[currentRow].style.display="none",currentRow=(currentRow+1)%rows.length,rows[currentRow].style.display="block"}const rows=document.querySelectorAll(".row");let currentRow=0;setInterval(rotateText,2e3);
