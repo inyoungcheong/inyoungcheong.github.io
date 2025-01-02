8b922dad0958e01db7ddf945a0ab7b2995f1d4db
inyoungcheong <44386633+inyoungcheong@users.noreply.github.com>
2025-01-02 05:48:12 +0000

    Deploying to gh-pages from @ inyoungcheong/inyoungcheong.github.io@014d8470d2617ee6a8de9d9dba1d0f0ee8f5a501 🚀

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,55 @@
-function changeWord(){for(var r=wordArray[currentWord],e=currentWord==words.length-1?wordArray[0]:wordArray[currentWord+1],t=0;t<r.length;t++)animateLetterOut(r,t);for(t=0;t<e.length;t++)e[t].className="letter behind",e[0].parentElement.style.opacity=1,animateLetterIn(e,t);currentWord=currentWord==wordArray.length-1?0:currentWord+1}function animateLetterOut(r,e){setTimeout(function(){r[e].className="letter out"},80*e)}function animateLetterIn(r,e){setTimeout(function(){r[e].className="letter in"},340+80*e)}function splitLetters(r){var e=r.innerHTML;r.innerHTML="";for(var t=[],n=0;n<e.length;n++){var a=document.createElement("span");a.className="letter",a.innerHTML=e.charAt(n),r.appendChild(a),t.push(a)}wordArray.push(t)}var words=document.getElementsByClassName("word"),wordArray=[],currentWord=0;words[currentWord].style.opacity=1;for(var i=0;i<words.length;i++)splitLetters(words[i]);changeWord(),setInterval(changeWord,4e3);
+var words = document.getElementsByClassName('word');
+var wordArray = [];
+var currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+  splitLetters(words[i]);
+}
+
+function changeWord() {
+  var cw = wordArray[currentWord];
+  var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
+  for (var i = 0; i < cw.length; i++) {
+    animateLetterOut(cw, i);
+  }
+  
+  for (var i = 0; i < nw.length; i++) {
+    nw[i].className = 'letter behind';
+    nw[0].parentElement.style.opacity = 1;
+    animateLetterIn(nw, i);
+  }
+  
+  currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
+}
+
+function animateLetterOut(cw, i) {
+  setTimeout(function() {
+		cw[i].className = 'letter out';
+  }, i*80);
+}
+
+function animateLetterIn(nw, i) {
+  setTimeout(function() {
+		nw[i].className = 'letter in';
+  }, 340+(i*80));
+}
+
+function splitLetters(word) {
+  var content = word.innerHTML;
+  word.innerHTML = '';
+  var letters = [];
+  for (var i = 0; i < content.length; i++) {
+    var letter = document.createElement('span');
+    letter.className = 'letter';
+    letter.innerHTML = content.charAt(i);
+    word.appendChild(letter);
+    letters.push(letter);
+  }
+  
+  wordArray.push(letters);
+}
+
+changeWord();
+setInterval(changeWord, 4000);
+
